91ee63870283a0adca4fd40cc89a9ba16eea87ae
K4isik <kaisar_kas@mail.ru>
2023-03-09 15:16:39 +0600

    regex

--- lab5.6.py
+++ lab5.6.py
@@ -1,6 +1,6 @@
 import re
-def snake_to_camel(s):
-    return re.sub(r'(?!^)_([a-zA-Z])', lambda m: m.group(1).upper(), s)
-snake_str = input() # as form 'hsds_wosncjs_hqywt'
-camel_str = snake_to_camel(snake_str)
-print(camel_str)  # as form 'hsdsWosncjsHqywt'
+a = input()
+res1 = re.sub(" ", ":", a)
+res1 = re.sub("[,]", ":", res1)
+res1 = re.sub("[.]", ":", res1)
+print(res1)
